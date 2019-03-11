12cc2a1480a3eadfa33705648c735151e3508696
jelen <jekibl@ttu.ee>
2019-03-11 11:07:11 +0200

    tests half done

--- src/test/java/ee/ttu/tarkvaratehnika/RecipeControllerTest.java
+++ src/test/java/ee/ttu/tarkvaratehnika/RecipeControllerTest.java
@@ -1,17 +1,13 @@
 package ee.ttu.tarkvaratehnika;
 
 import ee.ttu.tarkvaratehnika.controller.RecipeController;
-import ee.ttu.tarkvaratehnika.model.Recipe;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-
 import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.junit.Assert.assertTrue;
 
 
 @RunWith(SpringRunner.class)
@@ -26,10 +22,10 @@ public class RecipeControllerTest {
         assertThat(recipeController).isNotNull();
     }
 
-    @Test
-    public void postsShouldNotBeEmpty() {
-        new Recipe();
-        List<Recipe> recipes = recipeController.getRecipes(0);
-        assertTrue(recipes.size() != 0);
-    }
+//    @Test
+//    public void postsShouldNotBeEmpty() {
+//        new Recipe(1, "Name", "Description", "Ingredients", null);
+//        List<Recipe> recipes = recipeController.getRecipes(1);
+//        assertTrue(recipes.size() != 0);
+//    }
 }
