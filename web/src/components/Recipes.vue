106abe9d8b7d8ff1b900c1804de6ed5db5eb04d3
jelen <jekibl@ttu.ee>
2019-04-01 12:22:43 +0300

    small changes in recipes file

--- web/src/components/Recipes.vue
+++ web/src/components/Recipes.vue
@@ -3,11 +3,11 @@
         <div class="col-md-6">
             <h3>Recipes</h3>
             <ul>
-                <li class="recipe-item" v-for="recipe in recipes" :key=recipe.id>
+                <li v-for="(recipe, index) in recipes" :key="index">
                     <router-link :to="{
-                                name: 'recipe-details',
-                                params:{ recipe: recipe, id:recipe.id }}">
-                        {{recipe.recipeName}}
+                                name: 'recipe',
+                                params:{ recipe: recipe, id: recipe.id }}">
+                        {{recipe.name}}
                     </router-link>
                 </li>
             </ul>
@@ -16,8 +16,8 @@
             <router-view @refreshData="refreshList"></router-view>
         </div>
     </div>
-
 </template>
+
 
 <script>
     import http from "../http-common";
@@ -50,7 +50,7 @@
     h3 {
         margin-top:100px;
     }
-    .recipe-item {
+    recipe {
         border: 1px solid black;
         list-style-type: none;
         margin: 20px;
