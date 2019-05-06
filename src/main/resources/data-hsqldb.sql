0553ccabaa59ccbe44dcf1d13545ff951ba0f355
jelen <jekibl@ttu.ee>
2019-05-06 19:21:31 +0300

    data for recipes

--- src/main/resources/data-hsqldb.sql
+++ src/main/resources/data-hsqldb.sql
@@ -1,8 +1,36 @@
 insert into recipes(id, name, description, ingredients)
-values (1, 'Salat', 'Very Good', 'Cucumber, Tomato, Paprika, Cabbage.');
+values (1, 'Salat', 'Very Good.', 'Cucumber, Tomato, Paprika, Cabbage.');
 
 insert into recipes(id, name, description, ingredients)
-values (2, 'Cake', 'Very Sweet', 'Wheat Flour, Eggs, Sugar, Baking Powder.');
+values (2, 'Cake', 'Very Sweet.', 'Wheat Flour, Eggs, Sugar, Baking Powder.');
 
 insert into recipes(id, name, description, ingredients)
-values (3, 'Cocoa pudding', 'Good taste and easy to make', 'Cocoa powder, milk, potato starch.');
+values (3, 'Cocoa pudding', 'Good taste and easy to make.', 'Cocoa powder, milk, potato starch.');
+
+insert into recipes(id, name, description, ingredients)
+values (4, 'Strawberry Bruschetta', 'Usher in the warm days of spring and summer with Strawberry Bruschetta.',
+        'Strawberries, nectarine, olive oil, sugar, French bread baguette.');
+
+insert into recipes(id, name, description, ingredients)
+values (5, 'Tuna Wraps', 'Crunchy Avocado Tuna Wraps.', 'Tuna, avocado, carrot, celery, tortillas, lettuce, tomatoes');
+
+insert into recipes(id, name, description, ingredients)
+values (6, 'Sweet potato curry', 'Cook this tasty vegan curry for an exotic yet easy family dinner.',
+        'Sweet potato, coconut milk, spinach,  rice, peanut butter, curry paste.');
+
+insert into recipes(id, name, description, ingredients)
+values (7, 'Spaghetti Bolognese', 'Our best ever spaghetti Bolognese is super easy and a true Italian classic with a meaty, chilli sauce.',
+        'Beef mince, onions, tomatoes, spaghetti, parmesan.');
+
+insert into recipes(id, name, description, ingredients)
+values (8, 'One-pan salmon', 'For an easy side dish to complement a spring roast, just cook this recipe without the salmon.',
+        'Potato, asparagus spears, cherry tomatoes, salmon fillets.');
+
+insert into recipes(id, name, description, ingredients)
+values (9, 'Lemon cake', 'Lemon cake is a baking classic, packed with zesty flavour.',
+        'Butter, sugar, flour, lemons, eggs, natural yoghurt.');
+
+insert into recipes(id, name, description, ingredients)
+values (10, 'Pumpkin soup', 'Whip up this easy pumpkin soup as a starter for a dinner party.',
+        'Olive oil, onions, pumpkin, double cream.');
+
